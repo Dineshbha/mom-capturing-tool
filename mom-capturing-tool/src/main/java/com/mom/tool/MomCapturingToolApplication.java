abdca36e9c999b359448faa702a592d2f62c7767
Dinesh Bhatt <dinesh.bhatt@india.nec.com>
2020-03-08 07:58:16 +0530

    Initial commit

---
+++ mom-capturing-tool/src/main/java/com/mom/tool/MomCapturingToolApplication.java
@@ -0,0 +1,13 @@
+package com.mom.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomCapturingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomCapturingToolApplication.class, args);
+	}
+
+}
